8a825473fd35b2d19209d41e4a946cfe578809ce
Tobias Böhm <aibo@aibor.de>
2012-03-11 15:18:41 +0100

    added proper date to link entries

--- _posts/_insertdate.sh
+++ _posts/_insertdate.sh
@@ -2,5 +2,6 @@
 
 for f in 201*.html; do
   date=$(sed -r 's/(.{10})-(.{2})(.{2})(.{2})/\1 \2:\3:\4/' <<< ${f%.html})
-    sed -i '4i date: '"$date" $f
+    #sed -i '4i date: '"$date" $f
+    sed -ri '4 s/(.*)/\1-01:00/' $f
 done
